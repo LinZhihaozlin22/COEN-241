c8a1bacbd9d5dd4e1c1ccf95a2a93aebf66bed50
Zhihao Lin <lin.zhh99@outlook.com>
2022-04-20 13:14:19 -0700

    Delete HW1 directory
    replace with a new submission

--- HW1/bash_script/QEMU/CPU-test/CPU1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "CPU test 1 - 20000"
-for i in {1..5}
-do
-	echo "******* No. "$i
-	sysbench --test=cpu --cpu-max-prime=20000 --time=8 run
-done
--- HW1/bash_script/QEMU/fileIO-test/file2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "FileIO 2, Threads=16, size=4G"
-for i in {1..5}
-do
-	echo "****** No. "$i
-	sysbench --num-threads=16 --test=fileio --file-total-size=4G --file-test-mode=rndrw prepare
-	sysbench --num-threads=16 --test=fileio --file-total-size=4G --file-test-mode=rndrw run
-	sysbench --num-threads=16 --test=fileio --file-total-size=4G --file-test-mode=rndrw cleanup
-done
